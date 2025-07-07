2cdb947db99c298aa773152183a4007c9aa24b1a
sikara <xuhaoxian@gdlinkjob.com>
2025-07-07 15:56:49 +0800

    build: 降级 eslint 版本并优化 vite 配置
    将 eslint 从 9.x 降级至 8.38.0
    调整 vite.config.ts 中插件的导入顺序以提升可读性

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 // import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { visualizer } from 'rollup-plugin-visualizer';
 import viteCompression from 'vite-plugin-compression';
